1e987742bb4a69de787d4c7c64adb4b2192cd3f3
kakaosocks <kakaosocks@gmail.com>
2024-06-17 16:01:44 +0200

    Added Enemy Spawners

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,30 +2,66 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(NavMeshAgent), typeof(AgentLinkMover))]
 public class EnemyMovement : MonoBehaviour
 {
     public Transform Player;
     public float UpdateRate = 0.1f;
     private NavMeshAgent Agent;
+    private AgentLinkMover LinkMover;
+    [SerializeField]
+    private Animator Animator = null;
+
+    private const string IsWalking = "IsWalking";
+    private const string Jump = "Jump";
+    private const string Landed = "Landed";
+
+    private Coroutine FollowCoroutine;
 
     private void Awake()
     {
         Agent = GetComponent<NavMeshAgent>();
+        LinkMover = GetComponent<AgentLinkMover>();
+
+        LinkMover.OnLinkStart += HandleLinkStart;
+        LinkMover.OnLinkEnd += HandleLinkEnd;
     }
 
-    private void Start()
+    public void StartChasing()
     {
-        StartCoroutine(FollowTarget());
+        if (FollowCoroutine == null)
+        {
+            FollowCoroutine = StartCoroutine(FollowTarget());
+        }
+        else
+        {
+            Debug.LogWarning("Called StartChasing on Enemy that is already chasing! This is likely a bug in some calling class!");
+        }
     }
 
     private IEnumerator FollowTarget()
     {
         WaitForSeconds Wait = new WaitForSeconds(UpdateRate);
-        while(enabled)
+
+        while (gameObject.activeSelf)
         {
             Agent.SetDestination(Player.transform.position);
             yield return Wait;
         }
+    }
+
+    private void HandleLinkStart()
+    {
+        Animator.SetTrigger(Jump);
+    }
+
+    private void HandleLinkEnd()
+    {
+        Animator.SetTrigger(Landed);
+    }
+
+    private void Update()
+    {
+        Animator.SetBool(IsWalking, Agent.velocity.magnitude > 0.01f);
     }
 }
